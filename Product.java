854ade79f9f82a6243455ed290eea05fbef8ca6d
tinwok <tinworknguyen@gmail.com>
2026-09-04 09:07:21 +0700

    Product.java

--- Product.java
+++ Product.java
@@ -1,3 +1,30 @@
-public gi {
+import java.util.UUID;
+
+public class Product {
+    private String name;
+    private double price;
+    private final String id;
+
+    public Product(String name, double price) {
+        this.name = name;
+        this.price = price;
+        this.id = UUID.randomUUID().toString();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+    
+    public String getId() {
+        return id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
     
 }
